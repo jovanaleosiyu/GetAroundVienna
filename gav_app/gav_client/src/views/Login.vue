34e127c42a1e928c5e276fc0eba0486573f117ee
Siyu Sun <sun.s02@htlwienwest.at>
2021-11-23 07:52:52 +0100

    gitignore, eslint and login small changes

--- gav_app/gav_client/src/views/Login.vue
+++ gav_app/gav_client/src/views/Login.vue
@@ -1,47 +1,34 @@
-v-<template>
-    <div class="d-flex flex-column mt-16">
-        <v-btn
-        to="/welcome"
-        elevation="5"
-        icon
-        class="grey darken-3 white--text mb-3"
-        ><v-icon
-            left
-        >
-          mdi-arrow-left
-        </v-icon></v-btn>
-        <v-img
-        max-height="140"
-        max-width="140"
-        src="../assets/GAV-logo.svg"
-        class="mx-auto"
-        ></v-img>
-        <h1 class="text-center mt-8 mb-16">Willkommen<br>zurück!</h1>
-        <div class="mx-12" max-width="100">
-            <v-text-field
-            label="E-Mail"
-            ></v-text-field>
-            <v-text-field label="Passwort"></v-text-field>
-        </div>
-
-        <v-btn
-        elevation="5"
-        icon
-        class="grey darken-3 white--text"
-        ><v-icon
-            left
-        >
-          mdi-arrow-right
-        </v-icon>
-        </v-btn>
+v-
+<template>
+  <v-container class="d-flex flex-column align-center mt-16">
+    <v-btn
+      to="/welcome"
+      elevation="5"
+      icon
+      class="grey darken-3 white--text mb-3"
+    >
+      <v-icon> mdi-arrow-left </v-icon>
+    </v-btn>
+    <v-img
+      max-height="140"
+      max-width="140"
+      src="../assets/GAV-logo.svg"
+    ></v-img>
+    <h1 class="text-center mt-8 mb-16">Willkommen<br />zurück!</h1>
+    <div>
+      <v-text-field label="E-Mail"></v-text-field>
+      <v-text-field label="Passwort"></v-text-field>
     </div>
+
+    <v-btn elevation="5" fab class="align-self-end grey darken-3 white--text">
+      <v-icon> mdi-arrow-right </v-icon>
+    </v-btn>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    name: "Login",
-    data: () => ({
-
-    }),
-  }
+export default {
+  name: 'Login',
+  data: () => ({}),
+};
 </script>
