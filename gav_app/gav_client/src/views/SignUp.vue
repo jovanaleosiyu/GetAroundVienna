842c7d8dde9a4ae6c099b285a8b333dccf941dbc
LeopoldUrteil <urteil.l00@htlwienwest.at>
2021-11-24 21:30:00 +0100

    login dem mockup angepasst

--- gav_app/gav_client/src/views/SignUp.vue
+++ gav_app/gav_client/src/views/SignUp.vue
@@ -3,33 +3,39 @@ v-<template>
       <v-card class="d-flex flex-column align-center" :min-width="$vuetify.breakpoint.mdAndUp ? 450 :'100%'" outlined="flase" style="border: none">
 
         <div class="d-flex justify-start" style="width: 100%">
-        <v-btn
-          to="/welcome"
-          elevation="5"
-          icon
-          class="align-self-start grey darken-3 white--text mb-3"
-        >
-          <v-icon> mdi-arrow-left </v-icon>
-        </v-btn>
-       <v-img
-          max-height="46"
-          max-width="46"
-          src="../assets/GAV-logo.svg"
-          class="mx-auto"
-        ></v-img>
-        </div>
 
+          <v-btn
+            to="/welcome"
+            icon
+            class="my-auto"
+          >
+            <v-icon class="black--text" large> mdi-chevron-left </v-icon>
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-img
+            max-height="46"
+            max-width="46"
+            src="../assets/GAV-logo.svg"
+            class="mr-9"
+          ></v-img>
+
+          <v-spacer></v-spacer>
+        </div>
+        
         <h1 class="text-center mt-8 mb-16">Erstelle einen<br>Account</h1>
 
-        <div class="inputField">
+        <div style="width: 80%">
             <v-text-field label="E-Mail"></v-text-field>
             <v-text-field label="Passwort"></v-text-field>
             <v-text-field label="Passwort nochmal eingeben"></v-text-field>
         </div>
 
-        <v-btn elevation="5" fab class="align-self-end grey darken-3 white--text mt-16">
+        <v-btn elevation="4" fab class="align-self-end grey darken-3 white--text mt-16">
           <v-icon> mdi-arrow-right </v-icon>
         </v-btn>
+
       </v-card>
     </v-container>
 </template>
@@ -44,7 +50,4 @@ v-<template>
 </script>
 
 <style scoped>
-.inputField{
-  width: 80%;
-}
 </style>
--- gav_app/gav_client/src/views/Welcome.vue
+++ gav_app/gav_client/src/views/Welcome.vue
@@ -1,25 +1,27 @@
-v-
 <template>
-  <div class="d-flex flex-column mt-16">
+  <v-container class="d-flex flex-column align-center mt-16">
+
     <v-img
       max-height="140"
       max-width="140"
       src="../assets/GAV-logo.svg"
-      class="mx-auto"
     ></v-img>
+
     <h1 class="text-center mt-8 mb-16">Get Around<br />Vienna</h1>
-    <v-btn to="/login" class="grey darken-4 white--text mx-auto mb-4 rounded-pill"
-      >Anmelden</v-btn
-    >
 
-    <v-btn to="/sign-up" class="grey darken-4 white--text mx-auto mb-4 rounded-pill"
-      >Registrieren</v-btn
-    >
+    <v-btn to="/login"  width="257" class="grey darken-4 white--text mx-auto mb-4 rounded-pill">
+      Anmelden
+    </v-btn>
 
-    <router-link class="text-center mx-auto" to="#"
-      >Ohne Anmeldung fortfahren</router-link
-    >
-  </div>
+    <v-btn to="/sign-up" width="257" elevation="4" class="white white--black mx-auto mb-4 rounded-pill">
+      Registrieren
+    </v-btn>
+
+    <router-link class="black--text" to="#" style="text-decoration: none">
+      Ohne Anmeldung fortfahren
+    </router-link>
+
+  </v-container>
 </template>
 
 <script>
