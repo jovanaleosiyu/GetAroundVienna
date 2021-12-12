283947296ef4d0c8ded3f608c617676f7fb4c429
LeopoldUrteil <urteil.l00@htlwienwest.at>
2021-12-12 21:40:44 +0100

    Login mit validation

--- gav_app/gav_client/src/views/Login.vue
+++ gav_app/gav_client/src/views/Login.vue
@@ -1,7 +1,7 @@
 v-
 <template>
   <v-container class="d-flex flex-column align-center mt-16">
-    <v-card class="d-flex flex-column align-center" :min-width="$vuetify.breakpoint.mdAndUp ? 450 :'100%'" outlined="flase" style="border: none">
+    <v-card class="d-flex flex-column align-center" :outlined=true :min-width="$vuetify.breakpoint.mdAndUp ? 450 :'100%'" style="border: none">
 
       <div class="d-flex justify-start" style="width: 100%">
 
@@ -27,23 +27,61 @@ v-
 
       <h1 class="text-center mt-8 mb-16">Willkommen<br />zurück!</h1>
 
-      <div style="width: 80%">
-        <v-text-field label="E-Mail"></v-text-field>
-        <v-text-field label="Passwort"></v-text-field>
-      </div>
+      <v-form class="d-flex flex-column align-center" v-model="valid" style="width: 100%">
+        <div style="width: 80%">
+        <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-Mail"
+        required
+        ></v-text-field>
 
-      <v-btn elevation="5" fab class="align-self-end grey darken-3 white--text mt-16">
+        <v-text-field
+        v-model="password"
+        :rules="passwordRules"
+        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="show ? 'text' : 'password'"
+        @click:append="show = !show"
+        label="Passwort"
+        required
+        ></v-text-field>
+        </div>
+
+        <v-btn elevation="5" @click="login()" :disabled="!valid" fab class="align-self-end grey darken-3 white--text mt-16">
         <v-icon> mdi-arrow-right </v-icon>
       </v-btn>
+      </v-form>
 
     </v-card>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: 'Login',
-  data: () => ({}),
+  data: () => ({
+    valid: true,
+    email: '',
+    emailRules: [
+        v => !!v || 'E-mail ist erforderlich',
+        v => /.+@.+\..+/.test(v) || 'E-mail muss passend sein',
+      ],
+    password: '',
+    passwordRules: [v => !!v || "Passwort ist erforderlich"],
+    show: false
+  }),
+  methods: {
+    async login() {
+      var data = {
+        email: this.email,
+        password: this.password
+      }
+      this.email = '';
+      this.password = '';
+      await axios.post("http://localhost:3000/login",data)
+    }
+  },
 };
 </script>
 
