933b2e992322e52e3fb3af15490acc4b5afc48f3
jovanast1 <stevic.j02@htlwienwest.at>
2022-01-02 02:00:26 +0100

    favoriten seite angefangen

---
+++ gav_app/gav_client/src/views/Favorites.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card class="mx-auto" max-width="600" outlined>
+    <v-card-title>Favoriten</v-card-title>
+    <v-card-subtitle class="my-1">Haltestellen</v-card-subtitle>
+    <v-list class="d-flex flex-row">
+      <v-list-item>
+        <div class="text-center">
+          <v-btn icon elevation="3" large class="red">
+            <v-icon>mdi-home</v-icon>
+          </v-btn>
+          <p class="my-2">Home</p>
+        </div>
+      </v-list-item>
+      <v-list-item>
+        <div class="text-center">
+          <v-btn icon large elevation="3">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </div>
+      </v-list-item>
+    </v-list>
+    <v-card-subtitle>Routen</v-card-subtitle>
+    <v-list class="d-flex flex-row">
+      <v-list-item>
+        <div class="text-center">
+          <v-btn icon elevation="3" large class="red">
+            <v-icon>mdi-home</v-icon>
+          </v-btn>
+          <p>Home</p>
+        </div>
+      </v-list-item>
+      <v-list-item>
+        <div class="text-center">
+          <v-btn icon large elevation="3">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </div>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
