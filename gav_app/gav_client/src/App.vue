88e7cfc04af9bbf24aff4e0ddc924cb517b9133a
LeopoldUrteil <urteil.l00@htlwienwest.at>
2022-02-05 19:45:46 +0100

    test

--- gav_app/gav_client/src/App.vue
+++ gav_app/gav_client/src/App.vue
@@ -1,50 +1,40 @@
 <template>
   <v-app>
     <v-main>
-      <v-app-bar
-      color="gray"
-      dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
-    </v-app-bar>
+      <v-app-bar color="gray" dark>
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-toolbar-title>Title</v-toolbar-title>
+      </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="gray--text text--accent-4"
-        >
-          <v-list-item to="/home">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="gray--text text--accent-4"
+          >
+            <v-list-item to="/home">
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
 
-          <v-list-item to="/route">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Route</v-list-item-title>
-          </v-list-item>
+            <v-list-item to="/route">
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Route</v-list-item-title>
+            </v-list-item>
 
-          <v-list-item to="/favoriten">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Favoriten</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+            <v-list-item to="/favoriten">
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Favoriten</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
 
       <router-view />
     </v-main>
@@ -56,8 +46,8 @@ export default {
   name: "App",
 
   data: () => ({
-      drawer: false,
-      group: null,
-    }),
+    drawer: false,
+    test: "test",
+  }),
 };
 </script>
