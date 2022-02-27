f0cc416cbea0b5734711484255e482c7aba64daf
jovanast1 <stevic.j02@htlwienwest.at>
2022-02-28 00:09:31 +0100

    hinzufügen der haltestellen

--- gav_app/gav_client/src/components/Favorites_Stop.vue
+++ gav_app/gav_client/src/components/Favorites_Stop.vue
@@ -11,31 +11,58 @@
       <v-container class="justify-center" width="50">
         <v-row align="center" justify="center">
           <v-col cols="12" align="center">
-            <v-btn large icon class="black justify-center">
-              <v-icon color="white">mdi-star</v-icon>
-            </v-btn>
+            <v-menu offset-y>
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  large
+                  icon
+                  :class="iconcolor"
+                  v-model="iconcolor"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon color="white">mdi-{{ iconimage }}</v-icon>
+                </v-btn>
+              </template>
+              <div>
+                <v-btn
+                  icon
+                  small
+                  v-for="col in colors"
+                  :class="col"
+                  :key="col"
+                  @click="iconcolor = col"
+                ></v-btn>
+              </div>
+              <v-divider></v-divider>
+              <div>
+                <v-btn
+                  v-for="icon in icons"
+                  :class="icon"
+                  :key="icon"
+                  @click="iconimage = icon"
+                >
+                  <v-icon large color="black">mdi-{{ icon }}</v-icon>
+                </v-btn>
+              </div>
+            </v-menu>
           </v-col>
           <v-col cols="10">
-            <!-- <v-text-field
+            <v-text-field
               v-model="name"
               :rules="nameRules"
               label="Name"
               required
             ></v-text-field>
 
-            <v-text-field
-              v-model="address"
-              :rules="emailRules"
-              label="Adresse"
-              required
-            ></v-text-field> -->
+            <RouteInput title="Haltestelle" @setStop="setStop"></RouteInput>
           </v-col>
         </v-row>
       </v-container>
 
       <v-card-actions class="justify-center">
         <v-spacer></v-spacer>
-        <v-btn icon large class="grey darken-3" @click="dialog = false">
+        <v-btn icon large class="grey darken-3" @click="addStop">
           <v-icon color="white">mdi-check</v-icon>
         </v-btn>
       </v-card-actions>
@@ -44,13 +71,74 @@
 </template>
 
 <script>
+import RouteInput from './RouteInputField.vue';
+import axios from 'axios';
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'http://localhost:3000',
+});
 export default {
   data() {
     return {
       dialog: false,
+      colors: [
+        'red darken-1',
+        'purple darken-1',
+        'orange darken-1',
+        'green darken-1',
+        'cyan darken-1',
+        'deep-orange darken-3',
+      ],
+      icons: ['home', 'school', 'tree', 'food'],
+      iconcolor: '',
+      iconimage: '',
+      haltestelle: '',
+      name: '',
+      // title: 'Restaurant',
+      // icon: 'food',
+      // color: 'orange',
+      // ref: '16.32019:48.15985:WGS84',
+      // type: 'coord',
     };
+  },
+  components: {
+    RouteInput,
+  },
+  methods: {
+    async addStop() {
+      if (this.haltestelle) {
+        let stopData = {
+          title: this.name,
+          icon: this.iconimage,
+          color: this.iconcolor,
+          ref: this.haltestelle.ref,
+          type: this.haltestelle.type,
+        };
+        console.log(stopData);
+        await instance.post('/favorites/points', stopData);
+        this.dialog = false;
+        this.$emit('reload');
+      } else {
+        console.log('error');
+        console.log(this.haltestelle);
+      }
+    },
+    setStop(stop) {
+      console.log(stop);
+      if (stop.stopType === 'Haltestelle') this.haltestelle = stop;
+      else console.log('error');
+    },
+  },
+  created() {
+    this.iconcolor = this.colors[0];
+    this.iconimage = this.icons[0];
+    // bus.$emit('title', 'Favoriten');
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.favspace {
+  width: 15%;
+}
+</style>
