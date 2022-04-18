9c15b14fba936b2b9bfed3c0e4dacb48072da212
Siyu Sun <sun.s02@htlwienwest.at>
2022-04-18 23:16:49 +0200

    get stop monitors

--- gav_app/gav_server/model/stops.js
+++ gav_app/gav_server/model/stops.js
@@ -5,7 +5,12 @@ module.exports = {
   getStopList() {
     const uniqStopNames = [];
     for (const si of stopinfo) {
-      if (!uniqStopNames.includes(si.StopText)) uniqStopNames.push(si.StopText);
+      if (si.StopText && !uniqStopNames.includes(si.StopText)) {
+        uniqStopNames.push({
+          name: si.StopText,
+          DIVA: si.DIVA,
+        });
+      }
     }
     return uniqStopNames;
   },
